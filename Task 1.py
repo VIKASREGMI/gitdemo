3ccef1563290b6ebe7b96be6a23af1ac59f4e0a3
vikasregmi <122157462+VIKASREGMI@users.noreply.github.com>
2025-11-02 23:34:46 +0530

    Add files via upload
    This is Project one which is basic of python

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+Number1= float(input("First number: "))
+Number2= float(input("Second number: "))
+Addition=Number1+Number2
+print("Addition: ", Addition)
+Subtraction=Number1-Number2
+print("Subtraction: ", Subtraction)
+Multiplication=Number1*Number2
+print("Multiplication: ", Multiplication)
+Division=Number1/Number2
+print("Division: ",Division)
---
+++ Task 2.py
@@ -0,0 +1,7 @@
+from Lib.operator import concat
+
+F=str(input("Enter your first name: "))
+L=str(input("Enter your last name: "))
+Name=concat(F,L)
+print("Hello ",Name,"! Welcome to Python!")
+from Lib import operator
